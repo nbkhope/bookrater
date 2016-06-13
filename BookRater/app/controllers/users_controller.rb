b0d51b1c338628894b5ba10b70946bb56306799f
nbkhope <ryumemaru@yahoo.com>
2016-06-12 22:17:00 -0700

    Define actions in UsersController along with helper methods in private section

--- BookRater/app/controllers/users_controller.rb
+++ BookRater/app/controllers/users_controller.rb
@@ -1,22 +1,57 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    render json: @users
   end
 
   def show
+    @user = User.find(params[:id])
+    render json: @user
   end
 
-  def new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
-  def edit
-  end
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      err
+    end
   end
 
   def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      head :no_content
+    else
+      err
+    end
+
   end
 
   def destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.permit(:username, :email, :password)
+  end
+
+  def err
+    render json: @user.errors, status: :unprocessable_entity
   end
 end
