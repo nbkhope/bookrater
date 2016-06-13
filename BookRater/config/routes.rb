923982fb75f2e1c91a4cb2562ee016ce7d58083a
nbkhope <ryumemaru@yahoo.com>
2016-06-12 22:07:32 -0700

    Add skeleton for SessionsController

--- BookRater/config/routes.rb
+++ BookRater/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  post 'sessions/create'
+
+  delete 'sessions/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
