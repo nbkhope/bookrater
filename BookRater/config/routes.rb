b2c679f1e63250baa976fbd0ccd682f8e775b4ab
nbkhope <ryumemaru@yahoo.com>
2016-06-12 22:11:35 -0700

    Add skeleton for ReviewsController

--- BookRater/config/routes.rb
+++ BookRater/config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   delete 'sessions/destroy'
 
   resources :users
-  resources :books
+  resources :books do
+    resources :reviews
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
