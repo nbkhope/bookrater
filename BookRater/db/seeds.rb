e872c0dc3da7c771f0674aa68fe1f093e5f80b24
nbkhope <ryumemaru@yahoo.com>
2016-06-13 22:08:56 -0700

    Define seeds file to generate fake users, books, and reviews

--- BookRater/db/seeds.rb
+++ BookRater/db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Generate fake users
+30.times do 
+	user = User.create(
+		username: Faker::Internet.user_name,
+		email: Faker::Internet.email,
+		password: "1",#Faker::Internet.password,
+	)
+end
+
+# Generate fake books
+100.times do 
+	book = Book.create(
+		title: Faker::Book.title,
+		author: Faker::Book.author,
+		summary: Faker::Lorem.paragraph,
+		cover: "images/fake_book_cover" + ["", "2"].sample + ".png"
+	)
+
+	# Generate fake reviews
+	rand(0..5).times do 
+		Review.create(
+			content: Faker::Lorem.paragraph,
+			rating: rand(1..5),
+			user: User.all.sample,
+			book: book,
+		)
+	end
+end
+
