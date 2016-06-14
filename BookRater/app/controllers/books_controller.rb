5d8b04557533f6d4733352dbc9f05653c171ff55
nbkhope <ryumemaru@yahoo.com>
2016-06-13 22:20:21 -0700

    Define actions for BooksController

--- BookRater/app/controllers/books_controller.rb
+++ BookRater/app/controllers/books_controller.rb
@@ -1,22 +1,50 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
-  def new
-  end
+  # def new
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      render json: @book, status: :created, location: @book
+    else
+      err
+    end
   end
 
   def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      head :no_content
+    else
+      err
+    end
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    head :no_content
+  end
+
+  private
+
+  def book_params
+    params.permit(:title, :author, :summary, :cover)
+  end
+
+  def err
+    render json: @book.errors, status: :unprocessable_entity
   end
 end
