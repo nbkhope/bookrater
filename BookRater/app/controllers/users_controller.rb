385360fca94ca909cb4306ef3e4e72c4fcc012a4
nbkhope <ryumemaru@yahoo.com>
2016-06-13 22:38:36 -0700

    Correct before_action in UsersController (before_each is wrong)

--- BookRater/app/controllers/users_controller.rb
+++ BookRater/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_each :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: [:show, :update, :destroy]
 
   def index
     @users = User.all
