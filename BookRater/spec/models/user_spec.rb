cd0c078353726e8902dca7ac2aaca76444ed4659
nbkhope <ryumemaru@yahoo.com>
2016-06-29 08:38:26 -0700

    Add RSpec tests for User, Book, and Review models

--- BookRater/spec/models/user_spec.rb
+++ BookRater/spec/models/user_spec.rb
@@ -9,14 +9,18 @@ describe User, type: :model do
 	# test that the user's auth_token is unique
 	it { should validate_uniqueness_of(:auth_token) }
 
-	describe "#generate_authentication_token!" do
-		it "generates a unique token" do
+	# describe "#generate_authentication_token!" do
+	# 	it "generates a unique token" do
+	# 		Devise.stub(:friendly_token).and_return("auniquetoken123")
+	# 		@user.generate_authentication_token!
+	# 		expect(@user.auth_token).to eql "auniquetoken123"
+	# 	end
 
-		end
-
-		it "generates another token when one has already been taken" do
-
-		end
-	end
+	# 	it "generates another token when one has already been taken" do
+	# 		existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
+	# 		@user.generate_authentication_token!
+	# 		expect(@user.auth_token).not_to eql existing_user.auth_token
+	# 	end
+	# end
 
 end
