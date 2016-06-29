a3b9b2b84ec23ce9f8281f21955622089acc774c
nbkhope <ryumemaru@yahoo.com>
2016-06-28 19:03:07 -0700

    Add .rspec and spec/ directory with spec and rails helper; add skeleton for test for User model

---
+++ BookRater/spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe User do
+
+	describe "#generate_authentication_token!" do
+		it "generates a unique token" do
+
+		end
+
+		it "generates another token when one has already been taken" do
+			
+		end
+	end
+
+end
