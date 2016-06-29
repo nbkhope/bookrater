e2a026fa19570b549f40540f5d363dbef910e020
nbkhope <ryumemaru@yahoo.com>
2016-06-29 09:41:19 -0700

    Add delete_all to clean records before seeding; update password with 6-character password string; change all create statemenets to create! to display errors and not fail silently

--- BookRater/db/seeds.rb
+++ BookRater/db/seeds.rb
@@ -1,15 +1,19 @@
+Review.delete_all
+Book.delete_all
+User.delete_all
+
 # Generate fake users
 30.times do 
-	user = User.create(
+	user = User.create!(
 		username: Faker::Internet.user_name,
 		email: Faker::Internet.email,
-		#password: "1",#Faker::Internet.password,
+		password: "123456",#Faker::Internet.password,
 	)
 end
 
 # Generate fake books
 100.times do 
-	book = Book.create(
+	book = Book.create!(
 		title: Faker::Book.title,
 		author: Faker::Book.author,
 		summary: Faker::Lorem.paragraph,
@@ -18,7 +22,7 @@ end
 
 	# Generate fake reviews
 	rand(0..5).times do 
-		Review.create(
+		Review.create!(
 			content: Faker::Lorem.paragraph,
 			rating: rand(1..5),
 			user: User.all.sample,
@@ -30,7 +34,7 @@ end
 User.all.each do |user|
 	# Add favorite books to each user
 	rand(0..10).times do
-		Favorite.create(
+		Favorite.create!(
 			user: user,
 			book: Book.all.sample,
 		)
@@ -38,7 +42,7 @@ User.all.each do |user|
 
 	# Generate fake likes for reviews
 	rand(0..10).times do
-		Like.create(
+		Like.create!(
 			user: user,
 			review: Review.all.sample,
 		)
